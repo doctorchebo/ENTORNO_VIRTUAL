0279fba58e86fb56113daae3a180d2aa0dfaf7de
doctorchebo <marcelo.munoz.coaquira@gmail.com>
2022-03-16 15:29:54 -0400

    changing auditlog jsonfield

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     SECRET_KEY = config('SECRET_KEY')
