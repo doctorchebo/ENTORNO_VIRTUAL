8ec800c5deec0129f0203f960e07d2dc689cb654
doctorchebo <marcelo.munoz.coaquira@gmail.com>
2022-03-16 13:59:08 -0400

    comentando STATICFILES_DIRS

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -232,16 +232,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG == False:
+if not DEBUG :
     STATIC_ROOT = os.path.join(BASE_DIR,'static') 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-    os.path.join(BASE_DIR,'media')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static'),
+#     os.path.join(BASE_DIR,'media')
+# ]
 
 
 
