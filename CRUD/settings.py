1e6399ddc99797f466f80e9153b15d96ec6065b6
doctorchebo <marcelo.munoz.coaquira@gmail.com>
2022-03-17 11:37:39 -0400

    b

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -241,12 +241,11 @@ if not DEBUG :
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') 
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'static'),
-#     os.path.join(BASE_DIR,'media')
-# ]
-
-
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR,'static'),
+        os.path.join(BASE_DIR,'media')
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
